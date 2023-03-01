98e53780bb762d8127cef2fc7e395828881b6b05
kavery-pm <shiny.kavery@gmail.com>
2023-03-01 07:16:00 -0300

    added styling

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,12 @@
+import classes from './post.module.css';
 const Post = ({author,title})=>{
+
     const names=['shiny','kavery'];
     const chosenName = Math.random()> 0.5 ?names[0]:names[1]
     return (
-<div>
-    <p>{author}</p>
-    <p>{title}</p>
+<div className={classes.post}>
+    <p className={classes.author}>{author}</p>
+    <p className={classes.text}>{title}</p>
   
 </div>
     )
