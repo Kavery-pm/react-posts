1e71210bb29d6ca2b835b8450978ef35a91688e6
kavery-pm <shiny.kavery@gmail.com>
2023-03-09 06:32:26 -0300

    added the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import MainHeader from "./components/MainHeader";
 import Post from "./components/Post";
 import PostsList from "./components/PostsList";
 
 function App() {
   return <React.Fragment>
+    <MainHeader/>
     <PostsList/>
   
-    {/* <PostList  author='kavery' title='mayday'/> */}
+  
   </React.Fragment>
 }
 
