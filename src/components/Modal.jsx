8fdd82a261e531723d0b1b8aae0fa1decf6e40a9
kavery-pm <shiny.kavery@gmail.com>
2023-03-09 06:18:19 -0300

    added the modal handlers

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,8 @@
 import classes from './Modal.module.css'
-const Modal = ({children})=>{
+const Modal = ({children,onCloseModal})=>{
     return (
         <>
-        <div className={classes.backdrop}/>
+        <div className={classes.backdrop} onClick={onCloseModal}/>
         <dialog open className={classes.modal}>
 {children}
         </dialog>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,7 +4,7 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from './PostsList.module.css';
 const PostsList = ({author,title})=>{
-  
+  const [ismodalVisible, setismodalVisible] = useState(true)
     const [enteredtitle, setenteredtitle] = useState('');
     const [enteredAuthor, setenteredAuthor] = useState('')
     const textChangeHandler = (event)=>{
@@ -13,11 +13,17 @@ const PostsList = ({author,title})=>{
     const authorChangeHandler = (event)=>{
 setenteredAuthor(event.target.value);
     }
+    const closeModalHandler = ()=>{
+        setismodalVisible(false);
+    }
 return (
     <>
-    <Modal>
-    <NewPost handleTextAreachange={textChangeHandler} handleAuthorChange={authorChangeHandler}/>
-    </Modal>
+    {ismodalVisible && 
+    <Modal onCloseModal={closeModalHandler}>
+        <NewPost handleTextAreachange={textChangeHandler} handleAuthorChange={authorChangeHandler}/>
+    
+ 
+    </Modal>}
     <ul className={classes.posts}>
         <Post author={enteredAuthor} title={enteredtitle}/>
         <Post author='kavery' title='hayday'/>
