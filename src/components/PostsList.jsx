18492a4631a72d72b95fba643cfc84537aeee03f
kavery-pm <shiny.kavery@gmail.com>
2023-03-15 05:19:12 -0300

    added the loading state

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,51 +4,60 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 const PostsList = ({ onCloseModal, ismodalVisible }) => {
-
-
   const [posts, setposts] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
   useEffect(() => {
     fetchPosts();
-    }, [])
-  
-    const fetchPosts = async()=>{
-      const response =await fetch('http://localhost:8080/posts')
-      const resData = await response.json();
-         setposts(resData.posts)
-      }
-    
+  }, []);
+
+  const fetchPosts = async () => {
+    setisLoading(true);
+    const response = await fetch("http://localhost:8080/posts");
+    const resData = await response.json();
+    setposts(resData.posts);
+    setisLoading(false);
+  };
+
   const addPostHandler = (postData) => {
-    
-    fetch('http://localhost:8080/posts', {
-      method:'POST',
-      body:JSON.stringify(postData),
-      headers:{
-        'Content-type':'application/json'}
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-type": "application/json",
+      },
     });
-    setposts((existingPosts)=>[postData,...existingPosts]);
+    setposts((existingPosts) => [postData, ...existingPosts]);
   };
   return (
     <>
       {ismodalVisible && (
         <Modal onCloseModal={onCloseModal}>
-          <NewPost onCloseModal={onCloseModal} addPost={addPostHandler}/>
+          <NewPost onCloseModal={onCloseModal} addPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length>0 &&
-      <ul className={classes.posts}>
-        {posts.map((item) => (
-          <Post
-            author={item.authorName}
-            title={item.postBody}
-            key={posts.authorName}
-          />
-        ))}
-      </ul>}
-      {posts.length==0 && <div style={{textAlign:'center',color:'white'}}>
-        <h3>There are no posts</h3>
-        <p>Please start adding some...</p>
-        <p></p>
-        </div>}
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((item) => (
+            <Post
+              author={item.authorName}
+              title={item.postBody}
+              key={posts.authorName}
+            />
+          ))}
+        </ul>
+      )}
+      {!isLoading && posts.length == 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h3>There are no posts</h3>
+          <p>Please start adding some...</p>
+          <p></p>
+        </div>
+      )}
+      {isLoading && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>posts Loading....</p>
+        </div>
+      )}
     </>
   );
 };
