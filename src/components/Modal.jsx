bb034e4b2da99d1fbca943bb0cf358319c2e3acf
kavery-pm <shiny.kavery@gmail.com>
2023-03-15 06:40:12 -0300

    added the usenaviagte to close the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,18 @@
-import classes from './Modal.module.css'
-const Modal = ({children,onCloseModal})=>{
-    return (
-        <>
-        <div className={classes.backdrop} onClick={onCloseModal}/>
-        <dialog open className={classes.modal}>
-{children}
-        </dialog>
-        </>
-    )
-}
+import { useNavigate } from "react-router-dom";
+import classes from "./Modal.module.css";
+
+const Modal = ({ children }) => {
+  const navigate = useNavigate();
+  const closeModalHandler = () => {
+    navigate("/");
+  };
+  return (
+    <>
+      <div className={classes.backdrop} onClick={closeModalHandler} />
+      <dialog open className={classes.modal}>
+        {children}
+      </dialog>
+    </>
+  );
+};
 export default Modal;
