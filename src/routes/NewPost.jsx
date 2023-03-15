57804aeaeb266702aa9b2f0f13ed066d910171ac
kavery-pm <shiny.kavery@gmail.com>
2023-03-15 06:48:51 -0300

    added navigate to submit post

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Modal from "../components/Modal";
 import classes from "./NewPost.module.css";
-const NewPost = ({ onCloseModal, addPost }) => {
+const NewPost = ({ addPost }) => {
+  const navigate = useNavigate();
   const [enteredtitle, setenteredtitle] = useState("");
   const [enteredAuthor, setenteredAuthor] = useState("");
   const [postInfo, setpostInfo] = useState([]);
@@ -15,42 +16,42 @@ const NewPost = ({ onCloseModal, addPost }) => {
   const submitPostHandler = (event) => {
     event.preventDefault();
     let postData = { authorName: enteredAuthor, postBody: enteredAuthor };
-  
+
     setpostInfo(postData);
     addPost(postData);
-    onCloseModal();
+    navigate("/");
   };
 
   return (
     <Modal>
-    <form className={classes.form} onSubmit={submitPostHandler}>
-      <p>
-        <label htmlFor="text">Text</label>
-        <textarea
-          id="text"
-          required
-          rows={3}
-          onChange={textChangeHandler}
-          value={enteredtitle}
-        />
-      </p>
-      <p>
-        <label htmlFor="name">Your name</label>
-        <input
-          type="text"
-          id="name"
-          required
-          onChange={authorChangeHandler}
-          value={enteredAuthor}
-        />
-      </p>
-      <p className={classes.actions}>
-        <Link to='/' type="button" onClick={onCloseModal} >
-          Cancel
-        </Link>
-        <button>Submit</button>
-      </p>
-    </form>
+      <form className={classes.form} onSubmit={submitPostHandler}>
+        <p>
+          <label htmlFor="text">Text</label>
+          <textarea
+            id="text"
+            required
+            rows={3}
+            onChange={textChangeHandler}
+            value={enteredtitle}
+          />
+        </p>
+        <p>
+          <label htmlFor="name">Your name</label>
+          <input
+            type="text"
+            id="name"
+            required
+            onChange={authorChangeHandler}
+            value={enteredAuthor}
+          />
+        </p>
+        <p className={classes.actions}>
+          <Link to="/" type="button">
+            Cancel
+          </Link>
+          <button>Submit</button>
+        </p>
+      </form>
     </Modal>
   );
 };
